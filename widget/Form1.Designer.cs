4861bf2da205226be51203d9f4c2a1ecf5fed3b3
dysrea <atharvacn@gmail.com>
2026-05-18 16:37:08 +0530

    Add project files.

---
+++ widget/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace widget
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label2 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.None;
+            label2.AutoSize = true;
+            label2.Font = new Font("Aakriti", 71.99999F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(-33, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(873, 191);
+            label2.TabIndex = 1;
+            label2.Text = ")@ = )% = )(";
+            label2.Click += label2_Click;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Aakriti", 47.9999962F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.ImageAlign = ContentAlignment.TopCenter;
+            label1.Location = new Point(270, 279);
+            label1.Name = "label1";
+            label1.Size = new Size(439, 127);
+            label1.TabIndex = 2;
+            label1.Text = "Monday";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.Manual;
+            Text = "Form1";
+            TransparencyKey = Color.Black;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
---
+++ widget/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace widget
+{
+    public partial class Form1 : Form
+    {
+        // ---- THE "OWNER" WIN32 APIs ----
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern int SetWindowLong32(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+        private static extern IntPtr SetWindowLongPtr64(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        private const int GWL_HWNDPARENT = -8;
+        // --------------------------------
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.UseMnemonic = false;
+            label2.UseMnemonic = false;
+            
+            // -------------------------------------------------------------------
+
+            this.ShowInTaskbar = false;
+            this.Size = new Size(1300, 1000);
+            this.StartPosition = FormStartPosition.Manual;
+
+            label2.AutoSize = false;
+            label2.Size = new Size(1000, 180); // 160px height
+            label2.TextAlign = ContentAlignment.TopCenter; // Anchored to top!
+
+            label1.AutoSize = false;
+            label1.Size = new Size(930, 105); // 70px height
+            label1.TextAlign = ContentAlignment.TopCenter;
+
+            // -----------------------------------------------------
+
+            int screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            int xPosition = (screenWidth - this.Width) / 2;
+            int yPosition = (int)(screenHeight * 0.20);
+            this.Location = new Point(xPosition, yPosition);
+
+            timer1.Tick += Timer1_Tick;
+            UpdateClock();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            IntPtr desktopHandle = FindWindow("Progman", null);
+
+            if (desktopHandle != IntPtr.Zero)
+            {
+                if (IntPtr.Size == 8)
+                    SetWindowLongPtr64(this.Handle, GWL_HWNDPARENT, desktopHandle);
+                else
+                    SetWindowLong32(this.Handle, GWL_HWNDPARENT, desktopHandle.ToInt32());
+            }
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x800A0;
+                return cp;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateClock();
+        }
+
+        private void UpdateClock()
+        {
+            string rawTime = DateTime.Now.ToString("HH : mm : ss");
+            label2.Text = TranslateNumbersToAakriti(rawTime);
+
+            // 2. Process Day
+            string rawDay = DateTime.Now.ToString("dddd");
+            string finalDay = TranslateDayToAakriti(rawDay);
+
+            // 3. Process Date
+            string rawDate = DateTime.Now.ToString("dd . MM");
+            string finalDate = TranslateNumbersToAakriti(rawDate);
+
+            label1.Text = finalDay + "\u00A0\u00A0\u00A0\u00A0\u00A0" + finalDate;
+
+            label1.Left = (this.ClientSize.Width - label1.Width) / 2;
+            label2.Left = (this.ClientSize.Width - label2.Width) / 2;
+
+            label2.Top = 20;               // Time
+            label1.Top = 200;              // Day + Date combined line
+
+            label1.BringToFront();
+
+        }
+
+        private string TranslateNumbersToAakriti(string standardTimeOrDate)
+        {
+            var charMap = new Dictionary<char, char>
+            {
+                { '0', ')' }, { '1', '!' }, { '2', '@' }, { '3', '#' }, { '4', '$' },
+                { '5', '%' }, { '6', '^' }, { '7', '&' }, { '8', '*' },
+                { '9', '(' }, { ':', '：' }, { '.', '.' }
+            };
+
+            string translated = "";
+            foreach (char c in standardTimeOrDate)
+            {
+                if (charMap.ContainsKey(c))
+                    translated += charMap[c];
+                else
+                    translated += c;
+            }
+            return translated;
+        }
+
+        private string TranslateDayToAakriti(string englishDay)
+        {
+            var dayMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                { "Monday",    ";f]djf/" },
+                { "Tuesday",   "d+unjf/" },
+                { "Wednesday", "a'wjf/" },
+                { "Thursday",  "u'?jf/" },
+                { "Friday",    "z's|jf/" },
+                { "Saturday",  "zlgjf/" },
+                { "Sunday",    "/ljjf/" }
+            };
+
+            if (dayMap.ContainsKey(englishDay))
+                return dayMap[englishDay];
+
+            return englishDay;
+        }
+
+        private void label2_Click(object sender, EventArgs e) { }
+    }
+}
